24cee16ad5784d2d7de1d03b26f6c4092f12f30a
jesusHCG <48030764+jesusHCG@users.noreply.github.com>
2019-11-21 13:46:19 -0600

    Update PostBuildScript.ps1

--- DockerUtilities/PostBuildScript.ps1
+++ DockerUtilities/PostBuildScript.ps1
@@ -1,1 +1,21 @@
+<#
+   Date: 21/11/2019
+   Purpose: Post Build Script of Dynamo Primer
+#>
+$ErrorActionPreference = "Stop"
 
+try
+{
+	docker container stop build-primer
+	docker container rm build-primer
+}
+catch
+{
+	Write-Host $error[0]
+	throw $LASTEXITCODE
+}
+finally
+{
+	docker system prune -f
+	Invoke-Item "$env:WORKSPACE\DockerUtilities\RestartDockerDesktop.ps1"
+}
