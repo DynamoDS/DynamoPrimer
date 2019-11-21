8bb25dfb1411613bc9ebd680fedfb906762dfb8d
jesusHCG <48030764+jesusHCG@users.noreply.github.com>
2019-11-21 13:44:27 -0600

    Update PreBuildScript.ps1

--- DockerUtilities/PreBuildScript.ps1
+++ DockerUtilities/PreBuildScript.ps1
@@ -1,1 +1,21 @@
+<#
+   Date: 21/11/2019
+   Purpose: Pre Build Script of DynamoPrimer
+#>
+$ErrorActionPreference = "Stop"
 
+try
+{
+	
+	#Docker configuration
+	docker pull mcr.microsoft.com/windows:1903
+
+	docker run -m 8GB -d -t --mount type=bind,source=$env:WORKSPACE\DynamoPrimer,target=c:\WorkspacePrimer --name build-primer docker pull mcr.microsoft.com/windows:1903
+}
+catch
+{
+	docker system prune -f	
+	Invoke-Item "$env:WORKSPACE\DockerUtilities\RestartDockerDesktop.ps1"
+	Write-Host $error[0]
+	throw $LASTEXITCODE
+}
