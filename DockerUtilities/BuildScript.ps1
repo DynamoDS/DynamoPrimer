84ad9d99e2c4bb1eb207161381eb1f5f2a831ada
jesusHCG <48030764+jesusHCG@users.noreply.github.com>
2019-11-21 13:51:47 -0600

    Update BuildScript.ps1

--- DockerUtilities/BuildScript.ps1
+++ DockerUtilities/BuildScript.ps1
@@ -1,1 +1,19 @@
+<#
+   Date: 21/11/2019
+   Purpose: Build Script of Dynamo
+#>
+$ErrorActionPreference = "Stop"
 
+try
+{
+	$WorkSpaceElements = "$env:WORKSPACE" -split '\\'
+	$WorkSpaceName = $WorkSpaceElements[$WorkSpaceElements.Length - 1]
+	docker exec build-primer cmd "npm install gitbook-cli -g"
+}
+catch
+{
+	docker system prune -f	
+	Invoke-Item "$env:WORKSPACE\DockerUtilities\RestartDockerDesktop.ps1"
+	Write-Host $error[0]
+	throw $LASTEXITCODE
+}
