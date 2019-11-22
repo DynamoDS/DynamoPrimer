7e8030a6e4a1a3dea13ec53caebc3225e99fcba8
manuelsaldivar525 <48363605+manuelsaldivar525@users.noreply.github.com>
2019-11-22 09:48:23 -0600

    Update DockerCommands.ps1

--- DockerUtilities/DockerCommands.ps1
+++ DockerUtilities/DockerCommands.ps1
@@ -11,9 +11,12 @@ Copy-Item "$PrimerRoot\.npmrc" -Destination "C:\Users\ContainerAdministrator"
 
 npm install gitbook-cli -g
 
-Set-Location -Path "$PrimerRoot\en"
-
-gitbook init
-gitbook install
-gitbook build
-gitbook pdf . .\_book\Appendix\DynamoPrimer.pdf
+$array = @("de", "en", "ja","zh-tw")
+for ($i=0; $i -lt $array.length; $i++) {
+   $NewLocation = "$PrimerRoot" + "\" + $array[$i];
+   Set-Location $NewLocation
+   gitbook init
+   gitbook install
+   gitbook build
+   gitbook pdf . .\_book\Appendix\DynamoPrimer.pdf
+}
