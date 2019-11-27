a216c53fccce802accabd470419fd7e88d0546ea
manuelsaldivar525 <48363605+manuelsaldivar525@users.noreply.github.com>
2019-11-27 15:56:49 -0600

    Update DockerCommands.ps1

--- DockerUtilities/DockerCommands.ps1
+++ DockerUtilities/DockerCommands.ps1
@@ -8,13 +8,9 @@ $ErrorActionPreference = "Stop"
 
 # DynamoPrimer´s location
 $PrimerRoot = "c:\WorkspacePrimer"
-
-$array = @("de", "en", "ja","zh-tw")
-for ($i=0; $i -lt $array.length; $i++) {
-   $NewLocation = "$PrimerRoot" + "\" + $array[$i];
-   Set-Location $NewLocation
-   gitbook init
-   gitbook install
-   gitbook build
-   gitbook pdf . .\_book\Appendix\DynamoPrimer.pdf
-}
+$NewLocation = "$PrimerRoot" + "\" + $language;
+Set-Location $NewLocation
+gitbook init
+gitbook install
+gitbook build
+gitbook pdf . .\_book\Appendix\DynamoPrimer.pdf
