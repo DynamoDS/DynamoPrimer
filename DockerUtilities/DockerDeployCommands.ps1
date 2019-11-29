583913bc6fe4cdf70d7d0a3bbbcb3176ec34e6d1
t-hernjes <jesus.hc@globant.com>
2019-11-29 13:08:50 -0600

    Update DockerDeployCommands.ps1

--- DockerUtilities/DockerDeployCommands.ps1
+++ DockerUtilities/DockerDeployCommands.ps1
@@ -9,6 +9,18 @@ $ErrorActionPreference = "Stop"
 # DynamoPrimer´s location
 $PrimerRoot = "c:\WorkspacePrimer"
 
+#AWS variables
+$AWSPowerShellProfile = "MyNewProfile"
+$AWSRegion = "us-west-1"
+
+#AWS default configuration
+Initialize-AWSDefaultConfiguration -ProfileName $AWSPowerShellProfile -Region $AWSRegion
+
+#Set credentials
+Set-AWSCredential -ProfileName $AWSPowerShellProfile
+
+
+
 if($language == "en"){
    $array = @("Archive", "en", "gitbook","images", "styles", "index.html")
    
