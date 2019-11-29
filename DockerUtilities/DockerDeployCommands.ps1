ae5dada894faa1354dd4baf22a1d07bc5ea3f263
t-hernjes <jesus.hc@globant.com>
2019-11-29 13:12:09 -0600

    Update DockerDeployCommands.ps1

--- DockerUtilities/DockerDeployCommands.ps1
+++ DockerUtilities/DockerDeployCommands.ps1
@@ -12,14 +12,13 @@ $PrimerRoot = "c:\WorkspacePrimer"
 #AWS variables
 $AWSPowerShellProfile = "MyNewProfile"
 $AWSRegion = "us-west-1"
+$AWSBucketName = "testprimeglb"
 
 #AWS default configuration
 Initialize-AWSDefaultConfiguration -ProfileName $AWSPowerShellProfile -Region $AWSRegion
 
 #Set credentials
 Set-AWSCredential -ProfileName $AWSPowerShellProfile
-
-
 
 if($language == "en"){
    $array = @("Archive", "en", "gitbook","images", "styles", "index.html")
