10431a76c19baa68ac74588a4251869f4f59e088
t-hernjes <jesus.hc@globant.com>
2019-12-02 11:12:47 -0600

    Update DockerDeployCommands.ps1

--- DockerUtilities/DockerDeployCommands.ps1
+++ DockerUtilities/DockerDeployCommands.ps1
@@ -27,26 +27,30 @@ Initialize-AWSDefaultConfiguration -ProfileName $AWSPowerShellProfile -Region $A
 Set-AWSCredential -ProfileName $AWSPowerShellProfile
 
 
+#If not english delete everything with this language in the key
+$objectList = Get-S3Object -BucketName $AWSBucketName -Prefix "$language/"
+Write-Host "Deleting ..."
+foreach($myObject in $objectList){
+   Write-Host $myObject.Key
+   Remove-S3Object -BucketName $AWSBucketName -Key $myObject.Key -Force
+}
+Write-Host "Deletion complete!"
+
+#Get all files and upload
+$results = Get-ChildItem "$PrimerRoot\$language" -File -Recurse
+Write-Host "Uploading ..."
+foreach ($path in $results) {
+   $keyPath = $path.FullName.Replace("$PrimerRoot\$language\","").Replace("\","/")
+   Write-Host $keyPath
+   Write-S3Object -BucketName $AWSBucketName -File $path.FullName -Key "$language/$keyPath"
+}
+Write-Host "Upload complete!"
+
 if($language == "en"){
    $array = @("Archive", "en", "gitbook","images", "styles", "index.html")
    
-} else {
-   #If not english delete everything with this language in the key
-   $objectList = Get-S3Object -BucketName $AWSBucketName -Prefix $language
-   Write-Host "Deleting ..."
-   foreach($myObject in $objectList){
-      Write-Host $myObject.Key
-      Remove-S3Object -BucketName $AWSBucketName -Key $myObject.Key -Force
-   }
-   Write-Host "Deletion complete!"
-   
-   #Get all files and upload
-   $results = Get-ChildItem "$PrimerRoot\$language" -File -Recurse
-   Write-Host "Uploading ..."
-   foreach ($path in $results) {
-      $keyPath = $path.FullName.Replace("$PrimerRoot\$language\","").Replace("\","/")
-      Write-Host $keyPath
-      Write-S3Object -BucketName $AWSBucketName -File $path.FullName -Key "$language/$keyPath"
-   }
-   Write-Host "Upload complete!"
-}
+
+
+
+  
+} 
