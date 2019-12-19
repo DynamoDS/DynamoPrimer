406305db40a84993b2216381ddf1135f0ffd5f44
manuelsaldivar525 <48363605+manuelsaldivar525@users.noreply.github.com>
2019-12-18 18:22:08 -0600

    Jenkinsfile (#180)
    * Update DockerCommands.ps1
    
    Add compress pdf file with ghost script.
    
    * Update PreBuildScript.ps1
    
    Update image from 1.0.2 to 1.0.3.

--- DockerUtilities/DockerCommands.ps1
+++ DockerUtilities/DockerCommands.ps1
@@ -16,6 +16,8 @@ try
    gitbook install
    gitbook build
    gitbook pdf . .\_book\Appendix\DynamoPrimer.pdf
+   gswin64c.exe -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -dColorImageResolution=125dpi -sOutputFile=.\_book\Appendix\DynamoPrimer-Print.pdf  .\_book\Appendix\DynamoPrimer.pdf
+   Remove-Item -Path .\_book\Appendix\DynamoPrimer.pdf
 
 }
 catch
