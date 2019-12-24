09134c684c85d6341a354cca0df8b887bab5a390
manuel saldivar <manuel.saldivar@globant.com>
2019-12-24 15:23:56 -0600

    Update DockerPDFCompCommands.ps1

--- DockerUtilities/DockerPDFCompCommands.ps1
+++ DockerUtilities/DockerPDFCompCommands.ps1
@@ -6,6 +6,6 @@ $GhostScript = "$env:ProgramFiles\gs\gs9.50\bin\gswin64c.exe"
 $InputFile = "C:\WorkspacePrimer\$language\_book\Appendix\DynamoPrimer.pdf"
 $OutputFile = "C:\WorkspacePrimer\$language\_book\Appendix\DynamoPrimer-Print.pdf"
 
-& "$GhostScript" -dColorImageResolution=$pdfcompress_colorimageresolution -dPDFSETTINGS=/$pdfcompress_pdfsettings -dBATCH -dNOPAUSE -sDEVICE=$pdfcompress_device -sOutputFile="$OutputFile" "$InputFile"
+& "$GhostScript" -dColorImageResolution="$pdfcompress_colorimageresolution" -dPDFSETTINGS=/"$pdfcompress_pdfsettings" -dBATCH -dNOPAUSE -sDEVICE="$pdfcompress_device" -sOutputFile="$OutputFile" "$InputFile"
 
 Remove-Item -Force -Path $InputFile
