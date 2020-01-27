6a8c9cce830e237fcae23672e6070bccc7a5ddcf
manuel saldivar <manuel.saldivar@globant.com>
2020-01-27 16:09:38 -0600

    Add validations for empty languages.

--- DockerUtilities/DeployScript.ps1
+++ DockerUtilities/DeployScript.ps1
@@ -6,6 +6,10 @@ $ErrorActionPreference = "Stop"
 
 try
 {	
+	if ($env:ENVIRONMENT_LANGUAGE -eq "")
+	{
+		throw "No languages were selected."
+	}
 	docker exec build-primer powershell -command "C:\WorkspacePrimer\DockerUtilities\DockerDeployCommands.ps1 -a $env:ENVIRONMENT_LANGUAGE $env:BUCKETNAME $env:DISTRIBUTIONID"
 }
 catch
