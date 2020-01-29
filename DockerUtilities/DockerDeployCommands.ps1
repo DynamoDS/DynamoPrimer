20f0a87e179f2ec31d7dcb5d17330de65eef5324
alfredo-pozo <47230272+alfredo-pozo@users.noreply.github.com>
2020-01-29 11:25:09 -0800

    Changes to increase TTL and remove an alias on PS for better maintenance (#193)

--- DockerUtilities/DockerDeployCommands.ps1
+++ DockerUtilities/DockerDeployCommands.ps1
@@ -19,7 +19,7 @@ Function RemoveS3Folder {
    )
    if ([string]::IsNullOrEmpty($s3Prefix))
    {
-      $objectList = Get-S3Object -BucketName $AWSBucketName | Where $filter
+      $objectList = Get-S3Object -BucketName $AWSBucketName | Where-Object $filter
    }
    else
    {
@@ -65,7 +65,7 @@ Function UploadS3Folder {
 $PrimerRoot = "C:\WorkspacePrimer"
 
 #Vault
-$jsonToken = &vault write -address=https://civ1.dv.adskengineer.net:8200 -format=json /account/572569678988/sts/Application-Ops ttl=45m | ConvertFrom-Json
+$jsonToken = &vault write -address=https://civ1.dv.adskengineer.net:8200 -format=json /account/572569678988/sts/Application-Ops ttl=2h | ConvertFrom-Json
 Write-Host $jsonToken.request_id
 
 #AWS variables
