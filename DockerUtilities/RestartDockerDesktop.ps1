7bef2115d96e1f95afa5bebda54c88b45aefa33f
Aaron (Qilong) <173288704@qq.com>
2020-10-26 13:12:35 -0400

    Revert "Fix master primer (#245)" (#248)
    This reverts commit 45a28626e17c88c15e2c56f12daa913aade6a748.

---
+++ DockerUtilities/RestartDockerDesktop.ps1
@@ -0,0 +1,28 @@
+$ErrorActionPreference = "Stop"
+
+try 
+{
+
+    Write-Host "Looking for docker process"
+    $processes = Get-Process "*docker desktop*"
+    if ($processes.Count -gt 0)
+    {
+        Write-Host "Docker desktop found"
+        $processes[0].Kill()
+        $processes[0].WaitForExit()
+    }
+
+    Write-Host "Starting again Docker Desktop"
+    Start-Process "$env:ProgramFiles\Docker\Docker\Docker Desktop.exe" # -Verb RunAs
+
+    Write-Host "Giving some time to Docker desktop"
+    Start-Sleep -Seconds 30
+
+    Write-Host "Cleaning the containers that are not started"
+    docker container prune -f
+}
+catch 
+{
+    Write-Host $error[0]
+	throw $LASTEXITCODE
+}
